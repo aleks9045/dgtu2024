05e8ee20fb40ba327447049b3a88dbbe9c8ac24c
aleks9045 <aleksey9045@gmail.com>
2024-10-26 20:44:43 +0300

    pydantic

--- backend/core/api/challenges/schemas.py
+++ backend/core/api/challenges/schemas.py
@@ -10,8 +10,6 @@ class ChallengeCreateSchema(BaseModel):
     desc: Optional[str]
     start: Optional[datetime]
     end: Optional[datetime]
-    photo: Optional[str]
-    file: Optional[str]
     accepted: bool = Field(default=False)
     type: Optional[str]
     creator: str
@@ -23,11 +21,10 @@ class ChallengePatchChema(BaseModel):
     id_ch: int
     name: str = Field(default=None)
     desc: Optional[str] = Field(default=None)
-    points: int = Field(default=None)
     start: Optional[datetime] = Field(default=None)
     end: Optional[datetime] = Field(default=None)
-    photo: Optional[str] = Field(default=None)
-    file: Optional[str] = Field(default=None)
     accepted: bool = Field(default=False)
     type: Optional[str] = Field(default=None)
     creator: Optional[str] = Field(default=None)
+    title: str = Field(title="Achievement")
+    points: int = Field(title="Achievement")
