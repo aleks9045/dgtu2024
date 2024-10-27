52b8ca94a0a9de6d004ff67a346d46e092ae9664
aleks9045 <aleksey9045@gmail.com>
2024-10-27 04:15:44 +0300

    four joins

--- backend/core/api/challenges/routers.py
+++ backend/core/api/challenges/routers.py
@@ -47,7 +47,7 @@ async def get_challenges_by_user(schema: ChallengesIdSchema,
                                  payload: dict = Depends(verify_token),
                                  session: AsyncSession = Depends(db_session.get_async_session)) -> JSONResponse:
     result = await session.execute(
-        select(BaseUserModel.id_bu).where(
+        select(BaseUserModel.id_bu, UserModel.id_u, UserChallModel.id_ch).where(
             ChallengesModel.id_ch == schema["id_ch"]).join(UserChallModel,
                                                            ChallengesModel.id_ch == UserChallModel.id_ch).join(
             UserModel,
